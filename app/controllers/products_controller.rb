cb4e96b9a0e54e13190644324c0aa7336ccc7de0
Giorgio Zanni <g.zanni@hotmail.com>
2019-01-29 11:37:31 -0300

    ADD: input form

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,14 @@
 class ProductsController < ApplicationController
   def index
+    @product = Product.new
+  end
+
+  def create
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :price, :age)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'products/index'
+  get 'products/index', as: 'products'
+  post 'products/index', to: 'products#create', as: 'new_product'
 
   root 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
