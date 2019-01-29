9ef43b00c4f9dc162ddaef45dcc2f7e7f5e0978c
Giorgio Zanni <g.zanni@hotmail.com>
2019-01-29 12:24:37 -0300

    ADD: products table

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,14 +1,31 @@
 class ProductsController < ApplicationController
   def index
+    @products = Product.order(:id)
     @product = Product.new
   end
 
   def create
+    product = Product.new(product_params)
+    if product.save
+      redirect_to product, notice: 'Product saved!'
+    else
+      redirect_to root_path, alert: 'An error has ocurred, please try again.'
+    end
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to root_path, notice: 'Product was deleted.'
   end
 
   private
 
   def product_params
-    params.require(:product).permit(:name, :price, :age)
+    params.require(:product).permit(:name, :price, :description)
   end
 end
